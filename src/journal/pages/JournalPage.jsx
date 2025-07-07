486be02d7419f5e379033b34d7d334df495a124f
celizond <elizondo.celular@gmail.com>
2025-07-07 01:09:40 -0300

    create NothingSelectedView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,12 @@
 import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-        <Typography component="h1">JournalLayoutJournalLayoutJournalLayoutJournalLayoutJournalLayoutJournalLayoutJournalLayout JournalLayoutJournalLayoutJournalLayout JournalLayoutJournalLayoutJournalLayout JournalLayoutJournalLayoutJournalLayout JournalLayout JournalLayoutJournalLayoutJournalLayoutJournalLayoutJournalLayoutJournalLayout JournalLayoutJournalLayoutJournalLayoutJournalLayoutJournalLayoutJournalLayoutJournalLayout JournalLayout JournalLayoutJournalLayoutJournalLayoutJournalLayout Journal Page</Typography> 
+      <NothingSelectedView />
+        {/* <Typography component="h1">JournalLayoutJournalLayoutJournalLayoutJournalLayoutJournalLayoutJournalLayoutJournalLayout JournalLayoutJournalLayoutJournalLayout JournalLayoutJournalLayoutJournalLayout JournalLayoutJournalLayoutJournalLayout JournalLayout JournalLayoutJournalLayoutJournalLayoutJournalLayoutJournalLayoutJournalLayout JournalLayoutJournalLayoutJournalLayoutJournalLayoutJournalLayoutJournalLayoutJournalLayout JournalLayout JournalLayoutJournalLayoutJournalLayoutJournalLayout Journal Page</Typography>  */}
     </JournalLayout>
   )
 }
