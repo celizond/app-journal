42fada2f196354e0f2b99a487d3a7046b34edc51
celizond <elizondo.celular@gmail.com>
2025-07-07 01:39:16 -0300

    Update JournalPage.jsx

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,28 @@
-import { Typography } from "@mui/material"
+import { IconButton, Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NothingSelectedView } from "../views"
 import { NoteView } from "../views/NoteView"
+import { Add, AddOutlined } from "@mui/icons-material"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <NoteView />
-      {/* <NothingSelectedView /> */}
+      {/* <NoteView /> */}
+      <NothingSelectedView />
+
+      <IconButton
+        size="large"
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50, 
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
         {/* <Typography component="h1">JournalLayoutJournalLayoutJournalLayoutJournalLayoutJournalLayoutJournalLayoutJournalLayout JournalLayoutJournalLayoutJournalLayout JournalLayoutJournalLayoutJournalLayout JournalLayoutJournalLayoutJournalLayout JournalLayout JournalLayoutJournalLayoutJournalLayoutJournalLayoutJournalLayoutJournalLayout JournalLayoutJournalLayoutJournalLayoutJournalLayoutJournalLayoutJournalLayoutJournalLayout JournalLayout JournalLayoutJournalLayoutJournalLayoutJournalLayout Journal Page</Typography>  */}
     </JournalLayout>
   )
